c1fb32a905f31d2c36ef9d3055651983fdd770fe
mxTrivia <44403566+mxTrivia@users.noreply.github.com>
2018-10-23 16:18:41 +0300

    upload

---
+++ main — копия.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"C"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/proxy"
+	"io/ioutil"
+	"log"
+	"math"
+	"mxTrivia/Googler"
+	"mxTrivia/Qlive/gameid"
+	"mxTrivia/Qlive/getquestion"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+
+type clever struct {
+	Response struct {
+		Text    string `json:"text"`
+		Answers []struct {
+			ID   int    `json:"id"`
+			Text string `json:"text"`
+		} `json:"answers"`
+		SentTime int `json:"sent_time"`
+		ID       int `json:"id"`
+	} `json:"response"`
+}
+func vivod(inacheeslichtoto bool, procentstr1 string, procentstr2 string, procentstr3 string, variant1 string, variant2 string, variant3 string) (string, string, string){
+	var vivod1 = ""
+qwxeqwc
+			vivod1 = fmt.Sprintf("🛑 1. %s (%s)exqw, variant1, procentstr1)
+		}й < procentstr3 {йц
+		if procentstr3 < procentstr1 && procentstr3 < pчуйцroceйцч
+			vivod3 = fmt.Sprintf("🛑ecwqe 3. %s (%s)", variant3, procentstr3)
+		}ч
+	}
+xt
+			variant3 := res.Response.Answers[2].Text
+			var voprosnew, inacheeslichtoto = rabotasvoprosom(vopros)
+			var otvet1 = 0
+			var otvet2 = 0
+			var otvet3 = 0
+
+			if clevertelegram == true {
+				send_to_telegram("*Бот для игры Клевер:*\nВопрос - `" + vopros + "`\n\n" + vivod1 + "\n" + vivod2 + "\n" + vivod3, "@apihot_ch")
+				send_to_telegram("*Бот для игры Клевер:*\nqxe")
+			}
+
+	}
+}
+es[2].Choice
+			var voprosnew, inacheeslichtoto = rabotasvoprosom(vopros)
+			var otvet1 = 0
+			var otvet2 = 0
+			var
+		}
+		}
+func main() {
+}
